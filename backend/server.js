a0797894d6c29db4310aba281d94fa4c41ab5e5c
asim mian <asim.mian7878@gmail.com>
2025-01-13 20:26:13 +0000

    A

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL: ' + err.stack);
+    console.error('Error connecting to MySQL ' + err.stack);
     return;
   }
 

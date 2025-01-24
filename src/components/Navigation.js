4cfc666579f18e6981707f7dbe5e86a3096d4f49
asim mian <asim.mian7878@gmail.com>
2025-01-24 22:44:10 +0000

    Add Video Games link to Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -123,6 +123,11 @@ const Navigation = () => {
               MRI
               </a>
             </li>
+            <li>
+              <a href="/Videogames" className="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              Video Games
+              </a>
+            </li>
             
           </ul>
         </div>

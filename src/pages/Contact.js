e3f1f2d3f80aad322fb4f1b8f6046e8df9fd20e5
asim mian <asim.mian7878@gmail.com>
2025-01-27 17:00:44 +0000

    Refactor Contact form to manage state and improve submission handling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,45 +3,32 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function Contact() {
- 
+   const [setFormData, formData] = useState('');
+   const [setError, error] = useState('');
 
-    
+    // Handle input changes
+ const handleChange = (e) => {
+  setFormData({formData, [e.target.name]: e.target.value });
+
+
+// Handle form submission
+const handleSubmit = async () => {
+  const { name, email } = formData;
+};
   return (
-    <div className="bg-gray-50 font-[sans-serif]">
-    <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
-      <div className="max-w-md w-full">
-        <a href="javascript:void(0)">
-          <img
-            src="https://readymadeui.com/readymadeui.svg"
-            alt="logo"
-            className="w-40 mb-8 mx-auto block"
-          />
-        </a>
-    <div className="p-8 rounded-2xl bg-white shadow">
-      <h2 className="text-gray-800 text-center text-2xl font-bold">Contact</h2>
-      <form className="mt-8 space-y-4">
-        <label className="text-gray-800 text-sm mb-2 block">Name:
-          <input
-            type="text"
-            className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-            required
-          />
-        </label>
-        <label className="text-gray-800 text-sm mb-2 block">
-          Phone Number:
-          <input
-            type="number"
-            className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-            required
-          />
-        </label>
-        <button type="submit">Contact</button>
-      </form>
+   
+    <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+    <div className="mb-5">
+      <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name: </label>
+      <input type="text" id="name" value={formData.name} onChange={handleChange} className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="Name: " required />
     </div>
+    <div className="mb-5">
+      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email: </label>
+      <input type="email" id="email" className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="20273835@myclyde.ac.uk" required value={formData.email} onChange={handleChange}/>
     </div>
-    </div>
-    </div>
-  );
-}
 
-export default Contact;
+    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSubmit}>Register new account</button>      
+
+    </form>  
+)};
+} 

d151b39fc665270b9a558daaac71835f023d0086
asim mian <asim.mian7878@gmail.com>
2025-01-27 17:34:30 +0000

    Improve Map component layout by adding spacing and ensuring proper image rendering

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Map() {
   return (
+
     <>
     <div>Map</div>
     <img src="/assets/images/hospital_map.jpg" alt='Hospital_Map' />
     </> 
+    
   )
 }
 

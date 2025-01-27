555dd6ee52c8711f3d6c55fbe0bb9fb9df64bb18
asim mian <asim.mian7878@gmail.com>
2025-01-27 15:51:31 +0000

    Refactor Login form to manage form data and error state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,9 @@ function Login({ setIsLoggedIn }) {
   const [hospitalNumber, setHospitalNumber] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-
+  const [setFormData, formData] = useState('');
+  const [setError, error] = useState('');
+ 
  // Handle input changes
  const handleChange = (e) => {
   setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -47,15 +49,15 @@ const handleSubmit = async () => {
       </div>
       <div className="mb-5">
         <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name: </label>
-        <input type="name" id="name" value={formData.name} onChange={handleChange} className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="name@flowbite.com" required />
+        <input type="text" id="name" value={formData.name} onChange={handleChange} className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="Name: " required />
       </div>
       <div className="mb-5">
         <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-        <input type="email" id="email" className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="name@flowbite.com" required value={formData.email} onChange={handleChange}/>
+        <input type="email" id="email" className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" placeholder="20273835@myclyde.ac.uk" required value={formData.email} onChange={handleChange}/>
       </div>
       <div className="mb-5">
         <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-        <input type="password" id="password" className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" value={password} onChange={(e) => setPassword(e.target.value)}required />
+        <input type="text" id="password" className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder='Enter Password: '/>
       </div>
    
       <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSubmit}>Register new account</button>      

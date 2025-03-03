0016bcd50f95ba1e77ff8d54c6305b1cd2fd0b3a
asimmian12 <asim.mian7878@gmail.com>
2025-03-03 11:52:44 +0000

    Navigation should be finsished almost

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 const Navigation = ({ isLoggedIn, handleLogout }) => {
   const [userData, setUserData] = useState(null);
   const [isProfileOpen, setIsProfileOpen] = useState(false); 
-  // const age_restriction = isLoggedIn >= 6
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
@@ -55,10 +54,7 @@ const Navigation = ({ isLoggedIn, handleLogout }) => {
             <li><Link to="/dashboard" className="hover:bg-[#36336b] px-4 py-3 rounded-md block">Dashboard</Link></li>
             <li><Link to="/account" className="hover:bg-[#36336b] px-4 py-3 rounded-md block">Account</Link></li>
             <li><Link to="/About" className="hover:bg-[#36336b] px-4 py-3 rounded-md block">About</Link></li>
-            
-            {/* if ({age_restriction >= 6}) { */}
             <li><Link to="/games" className="hover:bg-[#36336b] px-4 py-3 rounded-md block">Games</Link></li>
-{/* } */}
             <li><Link to="/Map" className="hover:bg-[#36336b] px-4 py-3 rounded-md block">Map</Link></li>
             <li><Link to="/Appointment" className="hover:bg-[#36336b] px-4 py-3 rounded-md block">Appointment</Link></li>
             <li><Link to="/Doctor" className="hover:bg-[#36336b] px-4 py-3 rounded-md block">Doctor</Link></li>

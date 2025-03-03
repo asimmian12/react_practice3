bdbe66a7ce84953ee0e61bdcea61e0bf3200ddd2
asim mian <asim.mian7878@gmail.com>
2025-03-03 22:50:08 +0000

    removed image

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-// import Image from '../../assets/images/chest_xray.webp';
 
 const Dashboard = () => {
  

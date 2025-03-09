18c7a3648ef40d87afdd01b82479990eb4638aca
asim mian <asim.mian7878@gmail.com>
2025-03-09 19:55:51 +0000

    updating hosspital.sql

--- public/hospital.sql
+++ public/hospital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2025 at 03:12 AM
+-- Generation Time: Mar 09, 2025 at 08:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -68,8 +68,6 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `firstName`, `surname`, `dob`, `hospital_number`, `email`, `department_id`, `department_name`, `telephone_number`, `password`, `created_at`) VALUES
 (7, 'Asim', 'Mian', '2002-12-31', '1', '20273835@myclyde.ac.uk', 1, 'Cardiology', '0141 383 6089', '$2b$10$5QVGzx3rMq19Cht4h72p5uPSc41d0MNbQcY96VxM1l6NCE.zm3W9C', '2025-02-10 14:42:18'),
-(11, 'David', 'Stewart', '2002-12-30', 'CHI1234567890', 'asim.mian7878@gmail.com', 2, 'Neurology', '01413836089', 'Sajid365', '2025-02-10 15:33:28'),
-(16, 'Karen', 'Sturgon', '1333-12-12', '133', 'ksturgon@yahoo.com', 1, 'Cardiology', '01414445674', '$2b$10$Jev6NGmHXt3WYDYyQUbh7uMScSpsYBCDtK9q1QhRk/G7kMRPx9Hg.', '2025-02-16 18:06:57'),
 (17, 'Iain', 'Shaw', '1222-04-04', '124', 'ishaw@gmail.com', 2, 'Neurology', '01414532321', '$2b$10$VwrUNBmeykOK.cWmEj3zbupzdQgmX4qFMmLwfyvfJfYPqNlO5SSba', '2025-02-17 14:35:04');
 COMMIT;
 
